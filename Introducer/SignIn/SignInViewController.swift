f519b89b6b177db7bb80166654f42daf2d639e37
kamilporeba <kamil.poreba@icloud.com>
2016-03-19 21:42:05 +0100

    Create SignInViewController with outlets

--- Introducer/SignIn/SignInViewController.swift
+++ Introducer/SignIn/SignInViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class SignInViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    //Properties
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +28,8 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: Actions
+    @IBAction func signIn(sender: AnyObject) {
     }
-    */
 
 }
